4bc9a9610696877d405960cfffb3d7d69cde2dcb
Deepak Kumar <ruhani@Ruhanis-MacBook-Air.local>
2026-03-12 08:14:23 +0530

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/case-studies" element={<Case Studies />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
